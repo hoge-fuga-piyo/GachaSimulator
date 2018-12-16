b2e2f496824633571cfdeee6ca0db2d6bf320da0
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2018-12-17 01:36:00 +0900

    jsを修正

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,15 +1,15 @@
 function requestApi() {
     console.log("hogehoge");
     let request = new XMLHttpRequest();
-    request.open('get', '/v1/lottery?num=5&type1=0.5&type=0.5');
+    request.open('get', '/v1/lottery?num=5&type1=0.5&type2=0.5');
     request.onload = function(event) {
         let result = document.getElementById('result');
         result.innerHTML = "piyopiyo";
 
-        if (req.readyState == 4) {
-            if (req.status == 200) {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
                 console.log("SUCCESS");
-                result.innerHTML = req.responseText;
+                result.innerHTML = request.responseText;
             } else {
                 console.log("ERROR");
             }
