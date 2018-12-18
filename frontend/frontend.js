e3f4bfdfb61e2070234154cce4b886005ac8305a
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2018-12-19 00:00:52 +0900

    ガチャの結果をhtmlに反映させるよう修正

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,20 +1,35 @@
+function runLottery() {
+	let api_result = requestApi().done(function(result) {
+		console.log(JSON.stringify(result));
+		writeResult(JSON.stringify(result));
+	}).fail(function(result) {
+		console.log("failed call api");
+	});
+}
+
 function requestApi() {
-    console.log("hogehoge");
-    let request = new XMLHttpRequest();
-    request.open('get', '/v1/lottery?num=5&type1=0.5&type2=0.5');
-    request.onload = function(event) {
-        let result = document.getElementById('result');
-        result.innerHTML = "piyopiyo";
+	return $.ajax({
+		type: 'GET',
+		url: '/v1/lottery?num=10&type1=0.5&type2=0.3&type3=0.2',
+		dataType: 'json',
+	});
+}
 
-        if (request.readyState == 4) {
-            if (request.status == 200) {
-                console.log("SUCCESS");
-                result.innerHTML = request.responseText;
-            } else {
-                console.log("ERROR");
-            }
-        }
-    };
-
-    request.send(null);
+function writeResult(response) {
+	let result = JSON.parse(response).result;
+	let write_data = '';
+	for(let i=0; i < result.length; i++) {
+		if (result[i] == 0) {
+			write_data += "N<br>"
+			console.log("N");
+		} else if (result[i] == 1) {
+			write_data += "R<br>"
+			console.log("R");
+		} else {
+			write_data += "SR<br>"
+			console.log("SR");
+		}
+	}
+	console.log(write_data);
+	$('#result').html(write_data);
 }
